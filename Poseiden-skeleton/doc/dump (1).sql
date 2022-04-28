695c3632adaed135b85951ac0aed0bebc6c42d0f
ginochasles <ginochasles@gmail.com>
2022-04-28 11:59:12 +0200

    dump bdd, correct column's name ;)

---
+++ Poseiden-skeleton/doc/dump (1).sql
@@ -0,0 +1,255 @@
+DROP SCHEMA IF EXISTS `demo` ;
+CREATE SCHEMA IF NOT EXISTS `demo` DEFAULT CHARACTER SET utf8 ;
+USE `demo` ;
+
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demo
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bidlist`
+--
+
+DROP TABLE IF EXISTS `bidlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bidlist` (
+  `bid_list_id` int NOT NULL,
+  `account` varchar(255) DEFAULT NULL,
+  `ask` double NOT NULL,
+  `ask_quantity` double DEFAULT NULL,
+  `benchmark` varchar(255) DEFAULT NULL,
+  `bid` double NOT NULL,
+  `bid_list_date` datetime DEFAULT NULL,
+  `bid_quantity` double DEFAULT NULL,
+  `book` varchar(255) DEFAULT NULL,
+  `commentary` varchar(255) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `creation_name` varchar(255) DEFAULT NULL,
+  `deal_name` varchar(255) DEFAULT NULL,
+  `deal_type` varchar(255) DEFAULT NULL,
+  `revision_date` datetime DEFAULT NULL,
+  `revision_name` varchar(255) DEFAULT NULL,
+  `security` varchar(255) DEFAULT NULL,
+  `side` varchar(255) DEFAULT NULL,
+  `source_list_id` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `trader` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`bid_list_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bidlist`
+--
+
+LOCK TABLES `bidlist` WRITE;
+/*!40000 ALTER TABLE `bidlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bidlist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `curvepoint`
+--
+
+DROP TABLE IF EXISTS `curvepoint`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `curvepoint` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `as_of_date` datetime DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `curve_id` int DEFAULT NULL,
+  `term` double NOT NULL,
+  `value` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `curvepoint`
+--
+
+LOCK TABLES `curvepoint` WRITE;
+/*!40000 ALTER TABLE `curvepoint` DISABLE KEYS */;
+/*!40000 ALTER TABLE `curvepoint` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (3),(3),(3);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rating`
+--
+
+DROP TABLE IF EXISTS `rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rating` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fitch_rating` varchar(255) DEFAULT NULL,
+  `moodys_rating` varchar(255) DEFAULT NULL,
+  `order_number` int DEFAULT NULL,
+  `sand_p_rating` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rating`
+--
+
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rulename`
+--
+
+DROP TABLE IF EXISTS `rulename`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rulename` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `json` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `sql_part` varchar(255) DEFAULT NULL,
+  `sql_str` varchar(255) DEFAULT NULL,
+  `template` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rulename`
+--
+
+LOCK TABLES `rulename` WRITE;
+/*!40000 ALTER TABLE `rulename` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rulename` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trade`
+--
+
+DROP TABLE IF EXISTS `trade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trade` (
+  `trade_id` int NOT NULL,
+  `account` varchar(255) DEFAULT NULL,
+  `benchmark` varchar(255) DEFAULT NULL,
+  `book` varchar(255) DEFAULT NULL,
+  `buy_price` double DEFAULT NULL,
+  `buy_quantity` double DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `creation_name` varchar(255) DEFAULT NULL,
+  `deal_name` varchar(255) DEFAULT NULL,
+  `deal_type` varchar(255) DEFAULT NULL,
+  `revision_date` datetime DEFAULT NULL,
+  `revision_name` varchar(255) DEFAULT NULL,
+  `security` varchar(255) DEFAULT NULL,
+  `sell_price` double DEFAULT NULL,
+  `sell_quantity` double DEFAULT NULL,
+  `side` varchar(255) DEFAULT NULL,
+  `source_list_id` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `trade_date` datetime DEFAULT NULL,
+  `trader` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`trade_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trade`
+--
+
+LOCK TABLES `trade` WRITE;
+/*!40000 ALTER TABLE `trade` DISABLE KEYS */;
+INSERT INTO `trade` VALUES (1,'tilt2',NULL,NULL,0,1000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,NULL,NULL,NULL,NULL,NULL,'tilt2'),(2,'test',NULL,NULL,0,10,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,NULL,NULL,NULL,NULL,NULL,'test');
+/*!40000 ALTER TABLE `trade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Administrator','$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa','ADMIN','admin'),(2,'User','$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa','USER','user');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'demo'
+--
+
+--
+-- Dumping routines for database 'demo'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-28 11:54:49
